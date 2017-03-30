e2019c90dbb454c7a8761c983cc861af932acd25
Aron Adler <aronadler@hotmail.com>
2017-03-30 15:22:28 +0100

    HTML loader + npm build command

--- webpack.config.js
+++ webpack.config.js
@@ -1,3 +1,4 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
     entry: './src/index',
     output: {
@@ -6,12 +7,20 @@ module.exports = {
     },
     module: {
         rules: [{
-            test: /\.tsx?$/,
-            loader: 'ts-loader',
-            exclude: /node_modules/
-        }]
+                test: /\.tsx?$/,
+                loader: 'ts-loader',
+                exclude: /node_modules/
+            },
+            {
+                test: /\.html$/,
+                loader: 'html-loader'
+            },
+        ]
     },
     resolve: {
         extensions: ['.ts', '.tsx', '.js']
-    }
+    },
+    plugins: [new HtmlWebpackPlugin({
+        template: 'public/index.html'
+    })]
 };
